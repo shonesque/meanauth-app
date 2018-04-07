b2af39521e94eba3dae8127daa3e8830a38e0fc0
shonesque <cosmin@shonesquestudio.com>
2018-04-07 20:23:00 +0300

    repo init

---
+++ config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // configure the code below with your username, password and mlab database information
+  database: 'mongodb://cosmin:Superman123$@ds012058.mlab.com:12058/meanauthwebapp',   //prod
+  //database: 'mongodb://localhost:27017/meanauth',    //dev
+  secret: 'yoursecret'
+}
